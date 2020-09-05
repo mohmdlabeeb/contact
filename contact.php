f3e21ef24125236929d7eca079d6baaac051772e
mohmd labeeb <labeebshanavas@gmail.com>
2020-09-06 00:52:03 +0530

    initial commit

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+ 
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $subject = 'Website message';
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo ="contact@keralasurgicals.in";
+    $headers ="FROM: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    if(mail($mailTo,$subject,$txt,$headers)){
+              echo "Your Message has beeen sent";
+
+    }else{
+              echo "Failed to deliver your message";
+    }
+}
